302fa85dce8d175793d346d06d99acd8cdadd156
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2021-09-13 13:32:09 +0300

    Add files via upload

---
+++ Code-Challenge-Get-On-That-Ride/script.js
@@ -0,0 +1,10 @@
+function PlayOrNot(){
+var age = document.getElementById("age").value;
+var tall = document.getElementById("tall").value;
+
+if(age >=10 && tall >=52){
+    console.log("he can play");
+    window.alert("he can play");
+}
+}
+
