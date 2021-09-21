b979dd0886ab8f67bf59fb80dcf8dc1dbcc9ef1d
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2021-09-21 12:15:14 +0300

    Add files via upload

---
+++ Swift-Fundamentals-III/Aljadani_Rahaf_Swift_3.swift
@@ -0,0 +1,25 @@
+import Foundation
+//Write a program that adds the numbers 1-255 to an array
+var arr : [Int] = []
+for i in 1...255 {
+    arr.append(i)
+}
+//Swap two random values in the array
+arr.swapAt(Int.random(in: 1...255),Int.random(in : 1...255))
+//Now write the code that swaps random values 100 times (You've created a "Shuffle"!)
+for i in 1...100 {
+    arr.swapAt(Int.random(in: 1...255),Int.random(in: 1...255))
+}
+
+// Remove the value "42" from the array and Print "We found the answer to the Ultimate Question of Life,
+// the Universe, and Everything at index __" and print the index of where "42" was before you removed it.
+var j : Int = 0;
+for i in arr {
+    if (i == 42){
+    print("We found the answer to the Ultimate Question of Life, the Universe, and Everything at index \(j)")
+    print("\(arr[j])")//42
+    arr.remove(at: j)
+    break;
+    }
+    j+=1
+}
